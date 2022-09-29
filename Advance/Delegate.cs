8966378574a03c3d0468c6b0185d72a69cfb60fe
Divyansh174 <divyansh.pal@coditas.com>
2022-09-29 16:51:19 +0530

    29/09/2022

---
+++ Advance/Delegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance
+{
+    public delegate  int math(int x, int y);
+    internal class Delegate1
+    {
+        public int add(int x, int y) 
+        {
+            return x + y;   
+        }
+
+        public int sub(int x, int y) 
+        {
+            return x - y;    
+        }
+
+        public int divide(int x, int y) 
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else 
+            {
+                return x / y;    
+            }
+        }
+
+        public int multiply(int x, int y) 
+        {
+            return x * y;
+        }
+    }
+}
--- Advance/Program.cs
+++ Advance/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Advance;
+
+Delegate1 d = new Delegate1();
+
+math math1 = new math(d.add);
+
+math1 += d.sub;
+math1 += d.divide;
+math1 += d.multiply;
+foreach (math v in math1.GetInvocationList()) 
+{
+    try
+    {
+        int a = v(3, 0);
+        Console.WriteLine(a);
+    }
+    catch (Exception ex)
+    {
+
+        Console.WriteLine(ex.Message); 
+    }
+}
+
+
+
