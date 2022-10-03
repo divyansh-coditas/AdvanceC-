75f1ab2a4892b85063b3081a96c93cb73a78d992
Divyansh174 <divyansh.pal@coditas.com>
2022-10-03 17:08:28 +0530

    03/10/2022

---
+++ DelegateRealLife/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateRealLife
+{
+    public delegate bool DelegateRealLife1(Employee IsPromoted);
+    public class Employee
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public int salary { get; set; }
+
+        public int age { get; set; }
+
+        public void promtotion(List<Employee> employees, DelegateRealLife1 d1) 
+        {
+            foreach (var employee1 in employees) 
+            {
+                if (d1(employee1)) 
+                {
+                    Console.WriteLine($"{employee1.name} is promoted");
+                }
+            }    
+        }
+    }
+}
---
+++ DelegateRealLife/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using DelegateRealLife;
+Console.WriteLine("Hello, World!");
+DelegateRealLife1 d1 = new DelegateRealLife1(promoted);
+d1 += promoted1;
+Employee employee1 = new Employee() {id = 1, name = "Divyansh", salary = 500, age = 20};
+Employee employee2 = new Employee() { id = 2, name = "Abhisjhek", salary = 1000, age = 21 };
+List<Employee> Employees = new List<Employee>();
+
+Employees.Add(employee1);
+Employees.Add(employee2);
+
+Employee employee = new Employee();
+employee.promtotion(Employees, d1);
+
+
+static bool promoted(Employee employee) 
+{
+    if (employee.salary > 750)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+};
+
+static bool promoted1(Employee employee) 
+{
+    if (employee.age > 20)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
